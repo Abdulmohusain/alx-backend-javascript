91c670717f0edb960c5e679725d9fb081674fed8
Abdullahi Husain <abdulmohusain@gmail.com>
2024-07-03 08:28:04 +0000

    Const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+export const taskFirst = ()  => {
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export let  taskNext = () => {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
