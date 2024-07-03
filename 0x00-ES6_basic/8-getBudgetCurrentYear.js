9d04a71c383e746114c0574187551fb28dbbd17f
Abdullahi Husain <abdulmohusain@gmail.com>
2024-07-03 14:53:50 +0100

    ObjectPropertyShorthand

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,8 +1,3 @@
-function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
-
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
     [`income-${getCurrentYear()}`] = income;
