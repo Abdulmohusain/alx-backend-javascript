50a635b6184363eee08a9be844f223d1829423b1
Abdullahi Husain <abdulmohusain@gmail.com>
2024-08-02 12:37:04 +0100

    corrections

---
+++ 0x01-ES6_promise/ 1-promise.js
@@ -0,0 +1,13 @@
+export function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const data = {
+        status: 200,
+        body: 'success',
+      }
+      resolve(data);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
