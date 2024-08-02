a7f81b2e7b466309d47f62b7a8fa73a5cf4a36af
Abdullahi Husain <abdulmohusain@gmail.com>
2024-08-02 23:25:38 +0100

    ALX

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const ret = {
-     [departmentName]: employees
+    [departmentName]: employees,
   };
-  return ret
+  return ret;
 }
