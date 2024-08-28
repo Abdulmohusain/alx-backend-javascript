4a3fed1390eef542485601a1917c640ed0215a59
Abdullahi Husain <abdulmohusain@gmail.com>
2024-08-28 01:56:11 +0100

    ALx

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName: firstName,
+    lastName: lastName,
+  })
+}
